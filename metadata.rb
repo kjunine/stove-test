45365471ad1a1fd78abe7e4f420d86c1a8264702
Daniel Ku <kjunine@gmail.com>
2014-10-07 13:48:06 +0900

    Initial Commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'stove-test'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures stove-test'
+long_description 'Installs/Configures stove-test'
+version          '0.1.0'
+
